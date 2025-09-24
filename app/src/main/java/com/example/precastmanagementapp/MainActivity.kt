a94736ead871318137e2de5e8fbec0d6023b73e5
noaJ4Q <noe.jaraq@gmail.com>
2025-09-24 00:48:49 -0500

    init

--- app/src/main/java/com/example/precastmanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/precastmanagementapp/MainActivity.kt
@@ -1,30 +1,22 @@
 package com.example.precastmanagementapp
 
 import android.Manifest
-import android.app.Activity
-import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.precastmanagementapp.databinding.ActivityMainBinding
-import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
-@AndroidEntryPoint
+private const val PERMISSION_REQUEST_CODE = 1
+
 class MainActivity : AppCompatActivity() {
-
-    @Inject lateinit var bluetoothAdapter: BluetoothAdapter
 
     private lateinit var binding: ActivityMainBinding
 
