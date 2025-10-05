f9b4ac628f4e5a453e361a12cfee7a1c2e0bad09
noaJ4Q <noe.jaraq@gmail.com>
2025-10-05 17:50:22 -0500

    new refactoring

---
+++ app/src/main/java/com/example/precastmanagementapp/ble/BleExtensions.kt
@@ -0,0 +1,35 @@
+package com.example.precastmanagementapp.ble
+
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCharacteristic
+import android.util.Log
+
+fun BluetoothGatt.printGattTable() {
+    if (services.isEmpty()) {
+        Log.i("printGattTable", "No service and characteristic available, call discoverServices() first?")
+        return
+    }
+    services.forEach { service ->
+        val characteristicsTable = service.characteristics.joinToString(
+            separator = "n|--",
+            prefix = "|--"
+        ) { it.uuid.toString() }
+        Log.i("printGattTable", "nService ${service.uuid}nCharacteristics:n$characteristicsTable")
+    }
+}
+
+fun BluetoothGattCharacteristic.isReadable(): Boolean =
+    containsProperty(BluetoothGattCharacteristic.PROPERTY_READ)
+
+fun BluetoothGattCharacteristic.isWritable(): Boolean =
+    containsProperty(BluetoothGattCharacteristic.PROPERTY_WRITE)
+
+fun BluetoothGattCharacteristic.isWritableWithoutResponse(): Boolean =
+    containsProperty(BluetoothGattCharacteristic.PROPERTY_WRITE_NO_RESPONSE)
+
+fun BluetoothGattCharacteristic.containsProperty(property: Int): Boolean {
+    return properties and property != 0
+}
+
+fun ByteArray.toHexString(): String =
+    joinToString(separator = " ", prefix = "0x") { String.format("%02X", it) }
